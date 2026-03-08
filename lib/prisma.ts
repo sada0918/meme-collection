8f239de4e5aa551353ca18b986c9dd66adfe72ad
Shota Sada <117647259+sada0918@users.noreply.github.com>
2026-03-08 23:58:49 +0900

    Feature/setup prisma client (#3)
    * add: Prismaクライアントの作成に必要なライブラリを追加
    
    * add: PrismaClientを生成するロジックを追加
    
    * add: tsx直接実行のためのライブラリを追加
    
    * add: DBテーブルにデータを入れるためのファイルを追加
    
    * add: seed.tsをコマンドで実行するための設定を追加
    
    * add: build時にprismaクライアントのコードを生成するように修正

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+function createPrismaClient() {
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  return new PrismaClient({ adapter });
+}
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = createPrismaClient();
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = createPrismaClient();
+  }
+  prisma = globalForPrisma.prisma;
+}
+
+export default prisma;
