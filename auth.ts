7a95109ae223b70a135e5a6e9b7662c11e7fc49e
sada0918 <shota.0918.fw@gmail.com>
2026-03-11 01:29:35 +0900

    change: OAuthのプロバイダーにGithubを指定し、singInページを指定

--- auth.ts
+++ auth.ts
@@ -1,5 +1,11 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-});
+export const config = {
+  providers: [GitHub],
+  pages: {
+    signIn: "/admin/login",
+  },
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
