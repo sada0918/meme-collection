3d1865dc1c95ae87a66e8434bd43280f54af6191
sada0918 <shota.0918.fw@gmail.com>
2026-03-11 01:49:24 +0900

    change: 許可アカウントのみログインできるように修正

--- auth.ts
+++ auth.ts
@@ -1,10 +1,18 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const config = {
+export const config: NextAuthConfig = {
   providers: [GitHub],
   pages: {
     signIn: "/admin/login",
+  },
+  callbacks: {
+    signIn({ account, profile }) {
+      if (account?.provider === "github") {
+        return profile?.login === process.env.AUTH_ALLOWED_GITHUB_LOGIN;
+      }
+      return false;
+    },
   },
 };
 
