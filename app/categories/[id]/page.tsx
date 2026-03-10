65cf98af57dc5304fd3bcca8cfc27988ff63994f
Shota Sada <117647259+sada0918@users.noreply.github.com>
2026-03-10 22:38:39 +0900

    Feature/add-fetch-and-render-posts (#8)
    feat: Xからポストを取得して表示する

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -5,6 +5,7 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+
   const categoryId = parseInt(id, 10);
   const category = await prisma.category.findUnique({
     where: { id: categoryId },
@@ -12,11 +13,25 @@ export default async function Page({
   const posts = await prisma.post.findMany({
     where: { categoryId: categoryId },
   });
+
+  const embeds = await Promise.all(
+    posts.map(async (post) => {
+      const postCard = await fetch(
+        `https://publish.x.com/oembed?url=https://x.com/i/status/${post.postId}`,
+      );
+      const data = await postCard.json();
+      return data.html;
+    }),
+  );
+
   return (
     <div>
       カテゴリ名: {category?.name ?? "カテゴリが見つかりませんでした"}
-      {posts.map((post) => (
-        <div key={post.id}>{post.postId}</div>
+      {posts.map((post, index) => (
+        <div
+          key={post.id}
+          dangerouslySetInnerHTML={{ __html: embeds[index] }}
+        />
       ))}
     </div>
   );
