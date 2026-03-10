098608909ee5f0bc39c166bce8ced43352d9efae
sada0918 <shota.0918.fw@gmail.com>
2026-03-10 22:32:31 +0900

    add: ポストページへの遷移処理を追加

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import prisma from "../../lib/prisma";
 export default async function Page() {
   const categories = await prisma.category.findMany();
+
   return (
     <div>
       {categories.map((category) => (
-        <div key={category.id}>{category.name}</div>
+        <Link href={`/categories/${category.id}`} key={category.id}>
+          {category.name}
+        </Link>
       ))}
     </div>
   );
