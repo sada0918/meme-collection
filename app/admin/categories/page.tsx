cb3850b3e9a4304dc3d5be9a574411544d4c84dd
sada0918 <shota.0918.fw@gmail.com>
2026-03-14 18:52:33 +0900

    delete: ミドルウェアの判定ロジックを追加したことで各ページの認証チェックが不要になったため削除

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,10 +1,5 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import CategoryForm from "./CategoryForm";
 
 export default async function Page() {
-  const session = await auth();
-  if (!session) redirect("/admin/login");
-
   return <CategoryForm />;
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,3 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-
 export default async function Page() {
-  const session = await auth();
-  if (!session) redirect("/admin/login");
   return <>ログイン成功</>;
 }
