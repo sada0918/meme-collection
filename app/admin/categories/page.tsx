f43bbe6cca8ffa3111dc3e898e7fd66b23ca607b
sada0918 <shota.0918.fw@gmail.com>
2026-03-15 03:03:52 +0900

    add: 管理ページ間の移動をスムーズにするため、ルーティングを追加

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -3,5 +3,18 @@ import CategoryForm from "./CategoryForm";
 
 export default async function Page() {
   const categories = await prisma.category.findMany();
-  return <CategoryForm categories={categories} />;
+  return (
+    <div>
+      <h1>カテゴリ管理ページ</h1>
+      <ul>
+        <li>
+          <a href="/admin/">管理者トップ</a>
+          <a href="/admin/posts" style={{ marginLeft: "1rem" }}>
+            ポスト管理
+          </a>
+        </li>
+      </ul>
+      <CategoryForm categories={categories} />
+    </div>
+  );
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,15 @@
 export default async function Page() {
-  return <>ログイン成功</>;
+  return (
+    <div>
+      <h1>管理者トップページ</h1>
+      <ul>
+        <li>
+          <a href="/admin/categories">カテゴリ管理</a>
+          <a href="/admin/posts" style={{ marginLeft: "1rem" }}>
+            ポスト管理
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
