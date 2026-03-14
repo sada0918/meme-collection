934b376c4a51077911242e6caf18ba395450e7db
Shota Sada <117647259+sada0918@users.noreply.github.com>
2026-03-15 03:04:50 +0900

    Feature/add routing in admin (#17)
    add: 管理ページ間の移動をスムーズにするため、ルーティングを追加

--- app/admin/posts/page.tsx
+++ app/admin/posts/page.tsx
@@ -4,5 +4,18 @@ import PostForm from "./PostForm";
 export default async function Page() {
   const categories = await prisma.category.findMany();
 
-  return <PostForm categories={categories} />;
+  return (
+    <div>
+      <h1>ポスト管理ページ</h1>
+      <ul>
+        <li>
+          <a href="/admin/">管理者トップ</a>
+          <a href="/admin/categories" style={{ marginLeft: "1rem" }}>
+            カテゴリ管理
+          </a>
+        </li>
+      </ul>
+      <PostForm categories={categories} />
+    </div>
+  );
 }
