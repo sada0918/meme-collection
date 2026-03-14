d0f91472ad95f57cc9fbda831456596f04d7b060
Shota Sada <117647259+sada0918@users.noreply.github.com>
2026-03-14 19:02:59 +0900

    Feature/fix auth and nullcheck (#15)
    * fix: サーバー側の比較処理を厳密化
    
    * add: admin配下ページでセッションがない場合、ログイン画面に遷移する
    
    * delete: ミドルウェアの判定ロジックを追加したことで各ページの認証チェックが不要になったため削除

--- app/admin/posts/page.tsx
+++ app/admin/posts/page.tsx
@@ -1,12 +1,7 @@
-import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
-import { redirect } from "next/navigation";
 import PostForm from "./PostForm";
 
 export default async function Page() {
-  const session = await auth();
-  if (!session) redirect("/admin/login");
-
   const categories = await prisma.category.findMany();
 
   return <PostForm categories={categories} />;
