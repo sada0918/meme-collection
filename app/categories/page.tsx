eb2368c386db784fdbf13b6d3b1ac1da53653b37
sada0918 <shota.0918.fw@gmail.com>
2026-03-15 22:58:18 +0900

    style: カテゴリ画面にスタイルを追加

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import prisma from "../../lib/prisma";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default async function Page() {
   const categories = await prisma.category.findMany({
@@ -11,11 +14,33 @@ export default async function Page() {
   }
 
   return (
-    <div>
+    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 p-4">
       {categories.map((category) => (
-        <Link href={`/categories/${category.id}`} key={category.id}>
-          {category.name}
-        </Link>
+        <Card
+          className="mx-auto w-full max-w-sm overflow-hidden pt-0"
+          key={category.id}
+        >
+          <div className="relative aspect-video">
+            {category.imageUrl ? (
+              <Image src={category.imageUrl} alt={category.name} fill />
+            ) : (
+              <>
+                <span className="absolute inset-0 flex items-center justify-center text-sm text-muted-foreground">
+                  No Image
+                </span>
+                <div className="absolute inset-0 bg-black/25" />
+              </>
+            )}
+          </div>
+          <CardHeader>
+            <CardTitle>{category.name}</CardTitle>
+          </CardHeader>
+          <CardFooter>
+            <Button asChild className="w-full">
+              <Link href={`/categories/${category.id}`}>ポストを見る</Link>
+            </Button>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );
