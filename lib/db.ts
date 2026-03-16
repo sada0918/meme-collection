eb2304ad7aeb45b279d5d6a56d800719200e4294
sada0918 <shota.0918.fw@gmail.com>
2026-03-17 01:49:53 +0900

    delete: Previewも本番DBを見るように変更

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,6 @@ export function getConnectionString() {
   if (process.env.NODE_ENV === "test") {
     return process.env.DATABASE_URL_TEST;
   }
-  // VercelのPreview環境とローカル開発環境の両方でDATABASE_URL_DEVを使用する。
-  // VERCEL_ENVを使っている理由は、VercelのPreview環境のNODE_ENVがproductionになってしまうため。
-  if (process.env.VERCEL_ENV === "preview") {
-    return process.env.DATABASE_URL_DEV;
-  }
   if (process.env.NODE_ENV === "development") {
     return process.env.DATABASE_URL_DEV;
   }
